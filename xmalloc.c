061ae3c197d90ed79ec55da4c0d6fc0dea9e6974
stefantasic-1990 <stefantasic@outlook.com>
2024-08-06 17:32:12 -0400

    implement block size calculation and alignment

--- xmalloc.c
+++ xmalloc.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 
 #define MEMBLOCK_HEADER_SIZE sizeof(struct memBlock) // define the size of the memory block header
+#define WORD_SIZE sizeof(void*) // define the word size of the system to be the size of a pointer in bytes
 
 // memory block struct
 struct memBlock {
@@ -12,7 +13,12 @@ static struct memBlock* free_list = NULL; // global pointer to the free list (th
 
 // memory allocation function
 void* xmalloc(size_t size) {
+    // 0. if requested size is zero, return a NULL
+    if (size == 0) return NULL;
+
     // 1. adjust size to include header and align it
+    block_size = MEMBLOCK_HEADER_SIZE + size;
+    aligned_block_size = (block_size + WORD_SIZE - 1) & ~(WORD_SIZE - 1) // power of 2 alignment
 
     // 2. traverse the free list to find a suitable block
 
