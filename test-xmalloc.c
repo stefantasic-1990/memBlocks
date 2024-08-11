941557806fb5f22c8dfd346ecb287d2eb7a1f2aa
Stefan Tasic <stefantasic@outlook.com>
2024-08-11 11:07:22 -0400

    expand test cases, add 10 byte request

--- test-xmalloc.c
+++ test-xmalloc.c
@@ -19,17 +19,6 @@ int main() {
     printf("Word size is %zu bytes\n", WORD_SIZE);
     printf("MemBlock header size is %zu bytes\n\n", MEMBLOCK_HEADER_SIZE);
 
-    // test requesting 8 bytes, expecting to get an 8 byte block
-    void* ptr8 = xmalloc(8);
-    printf("Allocating 8 bytes\n");
-    if (ptr8 == NULL) {
-        printf("Allocation failed requesting 8 bytes");
-    } else {
-        printf("Allocation succeeded\n");
-        struct memBlock* block = (void*)(char*)ptr8 - MEMBLOCK_HEADER_SIZE;
-        printf("The size of this memory block is: %zu bytes\n\n", block->size);
-    }
-
     // test requesting 6 bytes, expecting to get an 8 byte block
     void* ptr6 = xmalloc(6);
     printf("Allocating 6 bytes\n");
@@ -41,10 +30,22 @@ int main() {
         printf("The size of this memory block is: %zu bytes\n\n", block->size);
     }
 
+    // test requesting 8 bytes, expecting to get an 8 byte block
+    void* ptr8 = xmalloc(8);
+    printf("Allocating 8 bytes\n");
+    if (ptr8 == NULL) {
+        printf("Allocation failed requesting 8 bytes");
+    } else {
+        printf("Allocation succeeded\n");
+        struct memBlock* block = (void*)(char*)ptr8 - MEMBLOCK_HEADER_SIZE;
+        printf("The size of this memory block is: %zu bytes\n\n", block->size);
+    }
+
     // test requesting 10 bytes, expecting to get a 16 byte block
-    void* ptr10 = xmalloc(6);
+    void* ptr10 = xmalloc(10);
+    printf("Allocating 10 bytes\n");
     if (ptr10 == NULL) {
-        printf("Allocation failed requesting 6 bytes");
+        printf("Allocation failed requesting 10 bytes");
     } else {
         printf("Allocation succeeded\n");
         struct memBlock* block = (void*)(char*)ptr10 - MEMBLOCK_HEADER_SIZE;
