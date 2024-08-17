f1e57e2a1bcaa4841e00a230e0bf5504bebe3da3
Stefan Tasic <stefantasic@outlook.com>
2024-08-16 21:51:22 -0400

    first draft template

--- ymalloc.c
+++ ymalloc.c
@@ -1,1 +1,34 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define MEMBLOCK_HEADER_SIZE sizeof(struct blockHeader)
+#define MEMBLOCK_FOOTER_SIZE sizeof(struct blockFooter)
+
+// memory block header
+struct blockHeader {
+    struct blockHeader* next;
+    size_t size;
+    bool free;
+};
+
+// memory block footer
+struct blockFooter {
+    size_t size;
+    bool free;
+};
+
+void* blockCoalesce(void* ptr) {
+    
+}
+
+void* blockSplit(void* ptr) {
+    
+}
+
+void* ymalloc(size_t size) {
+
+}
+
+void* yfree(void* ptr) {
+    
+}
