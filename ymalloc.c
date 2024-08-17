d237b5c737ee5752c1706775c1f7c39a4ec53bba
Stefan Tasic <stefantasic@outlook.com>
2024-08-17 09:12:52 -0400

    implement function that maps an arena of memory to the allocator

--- ymalloc.c
+++ ymalloc.c
@@ -4,6 +4,8 @@
 
 #define MEMBLOCK_HEADER_SIZE sizeof(struct blockHeader)
 #define MEMBLOCK_FOOTER_SIZE sizeof(struct blockFooter)
+#define ARENA_SIZE (16 * 1024 * 1024) // 16 MB
+#define WORD_SIZE sizeof(void*)
 
 // memory block header
 struct blockHeader {
@@ -20,8 +22,19 @@ struct blockFooter {
 
 struct blockHeader* free_list = NULL;
 
-void initializeMemory() {
+void* mapArena() {
+    void* arena = mmap(
+        NULL, 
+        ARENA_SIZE, 
+        PROT_READ | PROT_WRITE, 
+        MAP_PRIVATE | MAP_ANONYMOUS, 
+        -1, 0);
 
+    of (arena == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    return arena;
 }
 
 void* blockCoalesce(void* ptr) {
