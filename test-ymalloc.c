3ad0164da53fb84a6e8738914a4547ed33c88219
Stefan Tasic <stefantasic@outlook.com>
2024-08-18 13:25:25 -0400

    add basic allocation testing in script

--- test-ymalloc.c
+++ test-ymalloc.c
@@ -28,5 +28,24 @@ int main() {
     printf("The length of the system word is %zu bytes.\n", WORD_SIZE);
     printf("The size of the memory block header is %zu bytes.\n", MEMBLOCK_HEADER_SIZE);
     printf("The size of the memory block footer is %zu bytes.\n", MEMBLOCK_FOOTER_SIZE);
-    printf("The smallest possible block size is %zu bytes.\n", SMALLEST_BLOCK_SIZE);
+    printf("The smallest possible block size is %zu bytes.\n\n", SMALLEST_BLOCK_SIZE);
+
+    int bytes[] = {6, 10, 20, 100};
+    int bytes_len = sizeof(bytes) / sizeof(bytes[0]);
+    struct blockHeader* allocated_blocks[bytes_len];
+
+    for (int i = 0; i < bytes_len; i++) {
+        void* ptr = ymalloc(bytes[i]);
+        printf("Allocating %i bytes\n", bytes[i]);
+        if (ptr == NULL) {
+            printf("Allocation failed requesting %i bytes", bytes[i]);
+        } else {
+            printf("Allocation succeeded\n");
+            struct blockHeader* block = (void*)(char*)ptr - MEMBLOCK_HEADER_SIZE;
+            printf("The total size of this block is (header plus data): %zu bytes\n", (block->size + MEMBLOCK_HEADER_SIZE));
+            printf("The data size of this block is: %zu bytes\n\n", block->size);
+        }
+        allocated_blocks[i] = ptr;
+    }
+
 }
