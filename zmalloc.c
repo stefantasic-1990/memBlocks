17c2888c26f43e3f83ca8d3c54e10a1a2fe3e429
Stefan Tasic <stefantasic@outlook.com>
2024-09-18 08:31:32 -0400

    remove comments

--- zmalloc.c
+++ zmalloc.c
@@ -65,19 +65,3 @@ void init() {
         initialize_blocks(initialize_blocks(free_lists[i], small_block_data_size, small_block_amount), large_block_data_size, large_block_amount)
     }
 }
-
-/* NOTES
-- segregated free lists (segregated fit with specific class sizes (some powers of 2 classes)
-- min block size is the word size x2, so on 64 bit 16 bytes... and blocks are multiples of 16 so 16, 32, 48, 64,...
-- first fit placement strategy (if on the low end of the class size, search from front, otherwise from behind)
-  - stop the search if 
-- immediate block split, searching from the left, but split on multiples of 16 only
-- deferred coalesce, searching from the right, coalesce into multiples of 16 only
- - wax and wane as blocks are split and coalesced.
-*/
-
-/*
-- minimum block size is 16, blocks in general are multiples of 16
-- segregated free lists, each list contains a range of block sizes that are multiplels of 16
-- first fit placement strategy
-*/
